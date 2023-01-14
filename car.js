8fe2e2fc40e075942c32f21fc64ebb62dba19cc1
Sungjae Jung <sungjae.jung1031@gmail.com>
2023-01-14 21:56:12 +0900

    add traffics

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x,y,width,height){
+    constructor(x,y,width,height,controlType,maxSpeed=3){
         this.x=x;
         this.y=y;
         this.width=width;
@@ -7,30 +7,42 @@ class Car{
 
         this.speed=0;
         this.acceleration=0.2;
-        this.maxSpeed=3;
+        this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
         this.damaged=false;
 
-        this.sensor=new Sensor(this);
-        this.controls=new Controls();
+        if(controlType!="DUMMY"){
+            this.sensor=new Sensor(this);
+        }
+
+        this.controls=new Controls(controlType);
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         if (false==this.damaged) {
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
-            this.sensor.update(roadBorders);
+            this.damaged = this.#assessDamage(roadBorders,traffic);
         }
-        this.sensor.update(roadBorders);
+
+        if(this.sensor){
+            this.sensor.update(roadBorders,traffic);
+        }
+
     }
 
     // colligion algorithm reference
     // https://box2d.org/documentation/md__d_1__git_hub_box2d_docs_collision.html
-    #assessDamage(roadBorders){
+    #assessDamage(roadBorders,traffic){
         for(let i=0;i<roadBorders.length;i++){
             if(polysIntersect(this.polygon, roadBorders[i])){
+                return true;
+            }
+        }
+
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon, traffic[i].polygon)){
                 return true;
             }
         }
@@ -100,11 +112,12 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx){
+    draw(ctx,color){
         if(this.damaged){
             ctx.fillStyle="gray";
-        }else{
-            ctx.fillstyle="blue";
+        }
+        else{
+            ctx.fillStyle=color;
         }
 
         ctx.beginPath();
@@ -114,6 +127,8 @@ class Car{
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if(this.sensor){
+            this.sensor.draw(ctx);
+        }
     }
 }
